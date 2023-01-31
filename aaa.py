ff540ac37844d6d0acd5c1e8239cd79661187194
Nathan <nathanburnsds@gmail.com>
2023-01-31 14:47:18 -0500

    Adding functions

--- aaa.py
+++ aaa.py
@@ -5,8 +5,21 @@ word_list_5_letters = 'clash, sunks, nicer, musts, tatty, emote, build, bolds, g
 word_list_5_letters = [word.upper().strip() for word in word_list_5_letters]
 print(word_list_5_letters, '\n')
 
-def word_prioritizer(word_list):
-    output = []
+def subword_score(subword):
+    """
+    subword is a list of length 5. If the i'th letter is known, then it is filled in in the list. Otherwise it is '_'.
+    e.g. if the first letter is 'A', the third letter is 'D', but the others are unknown, then subword is ['A', '_', 'D', '_', '_'].
+    """
+
+
+def candidate_words(word_list, correct_word):
+
+    first_letter_candidates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    second_letter_candidates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    third_letter_candidates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    fourth_letter_candidates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    fifth_letter_candidates = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
     for word in word_list:
 
 
