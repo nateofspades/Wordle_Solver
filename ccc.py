2c2c167c814a767d12590c2265f1cb6eb394f4db
Nathan <nathanburnsds@gmail.com>
2023-01-31 22:52:02 -0500

    Adding Functions

---
+++ ccc.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+predicted_word = 'peels'
+colors = ['green', 'yellow', 'grey', 'green', 'grey']
+# correct_word = 'p__l_'
+
+# Identify which indices in predicted_word correspond to green letters.
+green_indices = []
+for i in range(5):
+    if colors[i] == 'green':
+        green_indices.append(i)
+
+# Subset the word_list to only include words which match the green letters at the same indices.
+# for index in green_indices:
+#     green_letter = predicted_word[index]
+#     word_list = [word for word in word_list if word[index] == green_letter]
+
+# Identify which indices in predicted_word correspond to grey or yellow letters.
+blank_indices = []
+for i in range(5):
+    if colors[i] != 'green':
+        blank_indices.append(i)
+
+# Subset the word_list to exclude words which match any of the yellow or grey letters at the same index.
+# for index in blank_indices:
+#     for word in word_list:
+#         if word[index] == predicted_word[index]:
+#             word_list.remove(word)
+
+# For the non-green predicted letters, counting how many yellows and how many greys there are for each.
+dict = {}
+for index_1 in blank_indices:
+    letter_1 = predicted_word[index_1]
+    if letter_1 not in dict:
+        dict[letter_1] = {'grey_count':0, 'yellow_count':0}
+        for index_2 in blank_indices:
+            letter_2 = predicted_word[index_2]
+            if letter_1 == letter_2:
+                if colors[index_2] == 'grey':
+                    dict[letter_1]['grey_count'] += 1
+                if colors[index_2] == 'yellow':
+                    dict[letter_1]['yellow_count'] += 1
+
+import pprint
+pprint.pprint(dict)
+
+# For each non-green predicted letter, if yellow_count = 1 then the number of occurrences of that letter in the blanks is grey_count.
+# Remove all words from word_list which do not satisfy this property.
+for word in word_list:
+    for letter in dict:
+        if dict[letter]['yellow_count'] == 1:
+
+
+
+
+# For each non-green predicted letter, if yellow_count = 0 then the number of occurrences of that letter is >= grey_count.
+# Remove all words from word_list which do not satisfy this property.
